76a4a5e0a9cdfe30e3be5c3b0878922ff00728b2
Saadia Bouloudene <saadiabouloudene@gmail.com>
2024-06-05 16:45:11 +0200

    begin langchain

---
+++ langchain_model.py
@@ -0,0 +1,5 @@
+#transcription de l'audio en texte, analyse des sentiments orchestré par langchain 
+
+#créer une chaine qui génére la transcription de l'audio en texte et analyse des sentiments 
+
+from langchain import LangChain
