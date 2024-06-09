0bc031f8e09e5084fd5aeadebb224693bf2bdf59
Saadia B <saadiabouloudene@gmail.com>
2024-06-09 14:42:44 +0200

    integration de azure openai et enregistrement analyse dans bdd

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,20 +2,23 @@
 #créer une chaine qui génére la transcription de l'audio en texte et analyse des sentiments 
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient
+from dotenv import load_dotenv 
+import openai
 import pymongo
-#from langchain import LangChain
 import os
+#from langchain import LangChain 
+
+load_dotenv() #charger les variables d'environnement à partir du fichier .env 
 
 #recuperer les variables d'environnement
 endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
 key = os.environ["AZURE_LANGUAGE_KEY"]
 
 #crée un objet azurekeycredential qui contient la clé d'abonnement et un objet textanalyticsclient qui contient l'endpoint et la clé
-credential = AzureKeyCredential(os.environ["AZURE_LANGUAGE_KEY"])
-text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+credential = AzureKeyCredential(key)
+text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
 
-#récupérer le document le plus récent de la collection
-#connect to mongodb 
+#connection à mongodb 
 mongo_uri = os.getenv('MONGO_URI')
 db_name = os.getenv('DB_NAME')
 collection_name = os.getenv('COLLECTION_NAME')
@@ -24,23 +27,15 @@ client = pymongo.MongoClient(mongo_uri)
 db = client[db_name]
 collection = db[collection_name]
 
-# Fetch the most recent document from the collection
+# obtenir le document le plus récent de la collection
 cursor = collection.find().sort([('_id', pymongo.DESCENDING)]).limit(1)
 document = cursor[0]
 
-# Analyze sentiment
-result = text_analytics_client.analyze_sentiment([document['text']], show_opinion_mining=True, show_stats=True)
+# Analise sentiment, par défaut c'est le nouveau modele qui est utilisé
+result = text_analytics_client.analyze_sentiment([document['text']], show_opinion_mining=True, show_stats=True, language='fr')
 doc = result[0]
 
-if not doc.is_error:
-    print(f"Document text: {document['text']}")
-    print(f"Overall sentiment: {doc.sentiment}")
-    print(f"Confidence scores: positive={doc.confidence_scores.positive:.3f}; neutral={doc.confidence_scores.neutral:.3f}; negative={doc.confidence_scores.negative:.3f}")
-
-else:
-    print(f"Error: {doc.error}")
-
-#add new field to the document in the collection 
+#enregistrer le sentiment de la conversation dans la base de données 
 collection.update_one(
     {'_id': document['_id']}, 
     {'$set':{'sentiment':doc.sentiment, 'confidence_scores':
@@ -48,6 +43,44 @@ collection.update_one(
               'negative': doc.confidence_scores.negative,
               'neutral': doc.confidence_scores.neutral}}})
 print("Sentiment analysis completed and saved to the database")
+
+
+#si le sentiment est négatif, apppliquer modele de azure openAI pour identifier la raison du sentiment négatif 
+openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
+openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT") # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+openai.api_type = 'azure'
+openai.api_version = '2023-05-15' 
+
+deployment_name="QSGC-gpt35"
+if doc.sentiment == 'negative':
+    #print("Negative sentiment detected. Identifying reasons...")
+    #prompt = 'Explique à partir de la réponse du client pourquoi il est insatisfait: '
+    #response = openai.ChatCompletion.create(engine=deployment_name, prompt=prompt, max_tokens=12)
+    #text = response['choices'][0]['text'].replace('\n', '').replace(' .', '.').strip()
+    #print(f"Voici la/les raisons qui expliquent l'insatisfaction du client:", text)
+    prompt = f"Le client a exprimé son insatisfaction dans le texte suivant: '{document['text']}'. En utilisant le texte, explique de façon concise les raisons de cette insatisfaction."
+    response = openai.Completion.create(
+        engine=deployment_name,
+        prompt=prompt,
+        max_tokens=100)
+    reason_text=response['choices'][0]['text'].strip()
+    print("$$$$$$VOICI LE/LES RAISON(S) DE L'INSATISFACTION DU CLIENT", reason_text)
+
+    #enregistrer les raisons identifiées dans la base de données
+    collection.update_one(
+        {'_id': document['_id']}, 
+        {'$set':{'reasons': reason_text}})
+    print("Reasons for negative sentiment saved to the database")
+
+if not doc.is_error:
+    print(f"Document text: {document['text']}")
+    print(f"Overall sentiment: {doc.sentiment}")
+    print(f"Confidence scores: positive={doc.confidence_scores.positive:.2f}; neutral={doc.confidence_scores.neutral:.2f}; negative={doc.confidence_scores.negative:.2f}")
+
+else:
+    print(f"Error: {doc.error}")
+
+
 
 #print last document in the collection 
 cursor = collection.find().sort([('_id', pymongo.DESCENDING)]).limit(1)
---
+++ test.py
@@ -0,0 +1,86 @@
+#transcription de l'audio en texte, analyse des sentiments orchestré par langchain 
+#créer une chaine qui génére la transcription de l'audio en texte et analyse des sentiments 
+### version avec huggingface qui prend input en anglais
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+from transformers import pipeline
+import pymongo
+import os
+#from langchain import LangChain 
+from dotenv import load_dotenv 
+
+load_dotenv() #charger les variables d'environnement à partir du fichier .env 
+
+#recuperer les variables d'environnement
+endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
+key = os.environ["AZURE_LANGUAGE_KEY"]
+
+#crée un objet azurekeycredential qui contient la clé d'abonnement et un objet textanalyticsclient qui contient l'endpoint et la clé
+credential = AzureKeyCredential(key)
+text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
+
+#récupérer le document le plus récent de la collection
+#connect to mongodb 
+mongo_uri = os.getenv('MONGO_URI')
+db_name = os.getenv('DB_NAME')
+collection_name = os.getenv('COLLECTION_NAME')
+
+client = pymongo.MongoClient(mongo_uri)
+db = client[db_name]
+collection = db[collection_name]
+
+# Fetch the most recent document from the collection
+cursor = collection.find().sort([('_id', pymongo.DESCENDING)]).limit(1)
+document = cursor[0]
+
+# Analyze sentiment par défaut c'est le nouveau modele qui est utilisé
+result = text_analytics_client.analyze_sentiment([document['text']], show_opinion_mining=True, show_stats=True, language='fr')
+doc = result[0]
+
+#si le sentiment est négatif, apppliquer un modele de LLM mistralai/Codestral pour identifier la raison du sentiment négatif
+from transformers import pipeline
+from transformers import PreTrainedTokenizerFast
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_id = "distilbert/distilgpt2"
+token = os.getenv('TOKEN')
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=token)    
+model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=token)
+
+if doc.sentiment == 'negative':
+    print("Negative sentiment detected. Identifying reasons...")
+    #model = pipeline('text-generation', model='mistralai/Codestral-22B-v0.1', token=os.getenv('TOKEN'))
+    text = document['text']
+    inputs = tokenizer(text, return_tensors="pt")
+
+    #tokenizer = PreTrainedTokenizerFast.from_pretrained('mistralai/Codestral-22B-v0.1')
+    outputs = model.generate(**inputs, max_new_tokens=20)
+    prompt = 'The following text has a negative sentiment: ' + text + " The reasons for this sentiment are: "
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+    #outputs = model(prompt, max_new_tokens=20)
+    print("Raisons identifiées:", tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+
+if not doc.is_error:
+    print(f"Document text: {document['text']}")
+    print(f"Overall sentiment: {doc.sentiment}")
+    print(f"Confidence scores: positive={doc.confidence_scores.positive:.2f}; neutral={doc.confidence_scores.neutral:.2f}; negative={doc.confidence_scores.negative:.2f}")
+
+else:
+    print(f"Error: {doc.error}")
+
+#add new field to the document in the collection 
+collection.update_one(
+    {'_id': document['_id']}, 
+    {'$set':{'sentiment':doc.sentiment, 'confidence_scores':
+             {'positive': doc.confidence_scores.positive, 
+              'negative': doc.confidence_scores.negative,
+              'neutral': doc.confidence_scores.neutral}}})
+print("Sentiment analysis completed and saved to the database")
+
+#print last document in the collection 
+cursor = collection.find().sort([('_id', pymongo.DESCENDING)]).limit(1)
+last_document = cursor[0]
+print(f"Last document in the collection: {last_document}")
