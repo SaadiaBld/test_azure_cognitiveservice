e8405f544831bb2b51bc2736ea3c2b660e423c04
Saadia B <saadiabouloudene@gmail.com>
2024-06-10 23:56:18 +0200

    ajout fonction asynchro séquence chaine

--- chaine.py
+++ chaine.py
@@ -1,14 +1,20 @@
+#from msilib import sequence
 import azure.cognitiveservices.speech as speechsdk
 from dotenv import load_dotenv
-from langchain.llms import AzureOpenAI
+#from langchain.llms import AzureOpenAI
+from langchain_openai import AzureOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+#from langchain.chains import LLMChain, SequentialChain, RunnableSequence
+from langchain_core.runnables import RunnableLambda
 import pymongo
 import os
+import asyncio
 
+
+load_dotenv()
 llm = AzureOpenAI()
 
-def transcription_audio_texte():
+def transcription_audio_texte(_):
     speech_key, service_region = os.getenv('SPEECH_KEY'), os.getenv('SERVICE_REGION')
     speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
     speech_config.speech_recognition_language="fr-FR"
@@ -41,7 +47,24 @@ def enregistrer_texte_mongodb(text_transcrit):
         result = collection.insert_one(document)
         print("Document inséré avec l'ID {}".format(result.inserted_id))
 
+def analyse_sentiment():
+    mongo_uri = os.getenv('MONGO_URI')
+    db_name = os.getenv('DB_NAME')
+    collection_name = os.getenv('COLLECTION_NAME')
+    client = pymongo.MongoClient(mongo_uri)
+    db = client[db_name]
+    collection = db[collection_name]
+    cursor = collection.find().sort([('_id', pymongo.DESCENDING)]).limit(1)
+    document = cursor[0]
+    return document
 
-chain=LLMChain([transcription_audio_texte, enregistrer_texte_mongodb])
-chain = SequentialChain([LLMChain(llm, transcription_audio_texte), LLMChain(llm, enregistrer_texte_mongodb)])
-chain("")
+# Crée une fonction asynchrone
+async def main():
+    runnable1=RunnableLambda(transcription_audio_texte)
+    runnable2=RunnableLambda(enregistrer_texte_mongodb)
+    sequence = runnable1 | runnable2
+    await sequence.ainvoke(None)
+    await llm.analyze_sentiment()
+
+# Exécute la fonction asynchrone
+asyncio.run(main())
